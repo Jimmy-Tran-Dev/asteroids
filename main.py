c8b2be3357eaae70e730415f958c0e2a9475899c
Jimmy-Tran-Dev <jimmytran61900@gmail.com>
2026-05-14 09:37:01 -0400

    Add player logidc

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -8,8 +9,19 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
     
+    
+    dt = 0
+    clock =pygame.time.Clock()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = updatable, drawable
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,7 +29,15 @@ def main():
                 return
         
         screen.fill("black")
+        
+        for drawing in drawable:
+            drawing.draw(screen)
+        updatable.update(dt)
+        
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time() / 1000.0
+        
 
 
 if __name__ == "__main__":
